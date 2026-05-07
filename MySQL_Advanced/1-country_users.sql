c7c93149a1200d5f0dcdc6dcdb22019f7ad441bb
alejandrocossio777-coder <alejandrocossio777-coder@users.noreply.github.com>
2026-05-07 17:07:51 -0600

    1-country_users.sql: Fixed typo

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -2,8 +2,8 @@
 -- Has a country 
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE; 
+	email VARCHAR(255) NOT NULL UNIQUE, 
 	name VARCHAR(255),
-	country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 	PRIMARY KEY (id)
 );
