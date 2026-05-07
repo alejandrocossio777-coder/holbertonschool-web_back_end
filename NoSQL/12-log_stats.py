f5daee5cb1fd3c917ce33d3f67a7918b2f616992
alejandrocossio777-coder <alejandrocossio777-coder@users.noreply.github.com>
2026-05-07 17:07:51 -0600

    12-log_stats.py: update file

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -16,10 +16,10 @@ def _count(coll, query=None):
 if __name__ == "__main__":
 	client = MongoClient('mongodb://127.0.0.1:27017')
 	collection = client.logs.nginx
-	
+
 	print("{} logs".format(_count(collection)))
 	print("Methods:")
-	print("\tmethod GET: {}".format(_count(collection, Ã{"method": "GET"})))
+	print("\tmethod GET: {}".format(_count(collection, {"method": "GET"})))
 	print("\tmethod POST: {}".format(_count(collection, {"method": "POST"})))
 	print("\tmethod PUT: {}".format(_count(collection, {"method": "PUT"})))
 	print("\tmethod PATCH: {}".format(_count(collection, {"method": "PATCH"})))
@@ -27,3 +27,4 @@ if __name__ == "__main__":
 	print("{} status check".format(
 		_count(collection, {"method": "GET", "path": "/status"})
 	))
+
