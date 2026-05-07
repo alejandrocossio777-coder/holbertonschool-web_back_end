f5fc9e3cb9440e6fd34ea756afddc8efa0ee6233
alejandrocossio777-coder <alejandrocossio777-coder@users.noreply.github.com>
2026-05-07 17:07:51 -0600

    12-log_stats.py: Refactor por problemas con checker

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,22 +2,21 @@
 """ Log stats """
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db_nginx = client.logs.nginx
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    count_logs = db_nginx.count_documents({})
-    print(f'{count_logs} logs')
+    print(f"{collection.count_documents({})} logs")
+    print("Methods:")
 
-    print('Methods:')
     for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
-        print(f'\tmethod {method}: {count_method}')
+        print(f"\tmethod {method}: {collection.count_documents({'method': method})}")
 
-    check = db_nginx.count_documents(
+    status = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
 
-    print(f'{check} status check')
+    print(f"{status} status check")
