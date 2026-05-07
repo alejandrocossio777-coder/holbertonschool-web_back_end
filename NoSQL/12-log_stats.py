1df98c8706348f851c2f7047c9cdba6728c77683
alejandrocossio777-coder <alejandrocossio777-coder@users.noreply.github.com>
2026-05-07 17:07:51 -0600

    12-log_stats.py: Test (?)

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
-""" 12-log_stats """
+""" Log stats """
 from pymongo import MongoClient
 
-
-def _count(coll, query=None):
-    """Count documents with compatibility across PyMongo versions."""
-    if query is None:
-        query = {}
-    try:
-        return coll.count_documents(query)
-    except Exception:
-        return coll.count(query)
-
-
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    db = client.logs
+    collection = db.nginx
 
-    print("{} logs".format(_count(collection)))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("{} logs".format(collection.count()))
     print("Methods:")
-    print("\tmethod GET: {}".format(_count(collection, {"method": "GET"})))
-    print("\tmethod POST: {}".format(_count(collection, {"method": "POST"})))
-    print("\tmethod PUT: {}".format(_count(collection, {"method": "PUT"})))
-    print("\tmethod PATCH: {}".format(_count(collection, {"method": "PATCH"})))
-    print("\tmethod DELETE: {}".format(_count(collection, {"method": "DELETE"})))
-    print("{} status check".format(
-        _count(collection, {"method": "GET", "path": "/status"})
-    ))
+
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method, collection.count({"method": method})
+        ))
+
+    status = collection.count({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
+
